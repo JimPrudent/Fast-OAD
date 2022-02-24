Corrected model for area_VT computation
Now the y1 considered is that of the first propeller.

models/handling_qualities/tail_sizing/compute_vt_area.py
@@ -41,7 +41,7 @@ class ComputeVTArea(om.ExplicitComponent):
         self.add_input("data:mission:sizing:main_route:cruise:altitude", val=np.nan, units="m")
         self.add_input("data:geometry:propulsion:engine:count", val=np.nan)
         self.add_input("data:propulsion:MTO_thrust", val=np.nan, units="N")
-        self.add_input("data:geometry:propulsion:nacelle:y", val=np.nan, units="m")
+        self.add_input("data:geometry:propulsion:propeller:y1", val=np.nan, units="m")
 
         self.add_output("data:geometry:vertical_tail:wetted_area", units="m**2", ref=100.0)
         self.add_output("data:geometry:vertical_tail:area", units="m**2", ref=50.0)
@@ -52,6 +52,7 @@ class ComputeVTArea(om.ExplicitComponent):
         self.declare_partials("data:geometry:vertical_tail:wetted_area", "*", method="fd")
         self.declare_partials("data:geometry:vertical_tail:area", "*", method="fd")
         self.declare_partials("data:aerodynamics:vertical_tail:cruise:CnBeta", "*", method="fd")
+        self.declare_partials("data:aerodynamics:vertical_tail:cruise:CnBeta_mot", "*", method="fd")
 
     def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
         # pylint: disable=too-many-locals  # needed for clarity
@@ -68,7 +69,7 @@ class ComputeVTArea(om.ExplicitComponent):
         altitude = inputs["data:mission:sizing:main_route:cruise:altitude"]
         n_engines = inputs["data:geometry:propulsion:engine:count"]
         thrust_sl = inputs["data:propulsion:MTO_thrust"]
-        y1 = inputs["data:geometry:propulsion:nacelle:y"]
+        y1 = inputs["data:geometry:propulsion:propeller:y1"]
 
         # Matches suggested goal by Raymer, Fig 16.20
         cn_beta_goal = 0.0569 - 0.01694 * cruise_mach + 0.15904 * cruise_mach ** 2

models/propulsion/custom_model/distributed_prop.py
@@ -47,12 +47,14 @@ class PROP_DISTRIB(om.ExplicitComponent):
         self.add_input("data:propulsion:MTO_thrust", val=np.nan, units="N")
         self.add_input("tuning:propeller:count", val=np.nan)
         self.add_input("data:geometry:wing:span", val=np.nan, units="m")
+        self.add_input("data:geometry:fuselage:maximum_width", val=np.nan, units="m")
 
         self.add_output("data:geometry:propulsion:propeller:diameter", units="m")
         self.add_output("data:propulsion:propeller:thrust_prop", units="N")
         self.add_output("data:aerodynamics:propulsion:propeller:speed_ejected", units="m/s")
         self.add_output("data:aerodynamics:propulsion:propeller:speed_disk", units="m/s")
         self.add_output("data:propulsion:propeller:power_tot", units="W")
+        self.add_output("data:geometry:propulsion:propeller:y1", units="m")
 
     def setup_partials(self):
             self.declare_partials("*", "*", method="fd")
@@ -62,6 +64,7 @@ class PROP_DISTRIB(om.ExplicitComponent):
         MTO = inputs["data:propulsion:MTO_thrust"]
         n_prop = inputs["tuning:propeller:count"]
         wing_span = inputs["data:geometry:wing:span"]
+        b_f = inputs["data:geometry:fuselage:maximum_width"]
 
         # Sizing at take-off
         takeoff_mach = 0.3
@@ -82,9 +85,10 @@ class PROP_DISTRIB(om.ExplicitComponent):
         Area_disk = np.pi * (D_pro/2)**2
         Speed_ejec = np.sqrt(2 * T_prop /(rho*Area_disk) + speed_0**2)
         Speed_disk = 0.5*(Speed_ejec + speed_0)
+        y1_prop = b_f/2 + 0.1 * D_pro
 
         # Error if span propellers bigger than wing_span
-        span_pro = D_pro * n_prop/2 * 1.1
+        span_pro = y1_prop + D_pro * n_prop/2 * 1.1
         if span_pro > (wing_span/2):
                 raise FastDistrPropulsionError(
                     "Error: there is no enough space for propellers on the wing."
@@ -95,3 +99,4 @@ class PROP_DISTRIB(om.ExplicitComponent):
         outputs["data:propulsion:propeller:power_tot"] = Power_tot
         outputs["data:aerodynamics:propulsion:propeller:speed_ejected"] = Speed_ejec
         outputs["data:aerodynamics:propulsion:propeller:speed_disk"] = Speed_disk
+        outputs["data:geometry:propulsion:propeller:y1"] = y1_prop